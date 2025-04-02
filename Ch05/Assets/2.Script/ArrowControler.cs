c382d5c6a11379f89ccb27af5063994de1d261c3
Norok <shrudfhr4264@gmail.com>
2025-04-02 15:43:22 +0900

    Ch05_0402

---
+++ Ch05/Assets/2.Script/ArrowControler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowControler : MonoBehaviour
+{
+    public float moveStep = 0.07f;
+    public float r1 = 0.5f;
+    public float r2 = 1.0f;
+    GameObject player;
+    
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        player = GameObject.Find("player");
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(0, -moveStep, 0); 
+
+        if (transform.position.y < -7f)
+        {
+            Destroy(gameObject); //자기 자신을 gameObject라고 함
+        }
+
+        //충돌 판정
+        Vector2 p1 = transform.position;
+        Vector2 p2 = player.transform.position;
+        Vector2 dir = p1 - p2;
+        float distance = dir.magnitude;
+
+
+        if(distance < r1 + r2)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Ch05/Assets/2.Script/PlayerControler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveStep = 1f;
+   
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-moveStep, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(moveStep, 0, 0);
+        }
+    }
+}
